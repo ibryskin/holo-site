bdff357016710572c8c5cb01d0731b95c83e97b3
Igor Bryskin <igor@mac-mini-igor.local.katharsis.ru>
2019-10-17 15:52:19 +0300

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+function openPicture(obj) {
+    var w = window.open(obj.href, "_hologram", "width=650,height=650,status=0,toolbar=0,menubar=0,location=0,directories=0,scrollbars=yes,top=0,left=0");
+    w.focus();
+}
